9630180e8eb61a4bec9cccf365b9b1b8193505da
Luis Silva <luis.silva@bemobi.com>
2026-01-20 11:29:13 -0300

    fix: add vercel config

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 // src/api/apiClient.ts
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient = axios.create({
   baseURL,
